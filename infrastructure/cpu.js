1e7e790f48e85b12134a956f052665cd27c2ba70
Guilherme <lgapontes@gmail.com>
2017-05-23 21:15:20 -0300

    Fix issues

--- infrastructure/cpu.js
+++ infrastructure/cpu.js
@@ -2,12 +2,20 @@
 
 const osUtils  = require('os-utils');
 const pusage = require('pidusage');
+const windowsCpu = require('windows-cpu');
+const os = require('os');
 
 var CpuOS = function() {
     this.getStatus = function(request,callback) {
         osUtils.cpuUsage(function(value){
             var cpuUsage = parseInt(value * 100);
             var cpuFree = 100 - cpuUsage;
+
+            if (cpuUsage > 100) {
+                cpuUsage = 100;
+                cpuFree = 0;
+            }
+
             callback(undefined,[
                 cpuUsage,
                 cpuFree
@@ -18,27 +26,56 @@ var CpuOS = function() {
 
 var CpuProcess = function() {
     this.getStatus = function(request,callback) {
-        pusage.stat(process.pid, function(err, stat) {
-            if (err) {
-                callback(err);
-            } else {
-                if (stat.cpu === undefined) {
-                    callback('CPU status not found!');
-                } else {
-                    console.log(stat.cpu);
-                    let cpuUsage = parseInt(stat.cpu)
-                    let cpuFree = 100 - cpuUsage;
 
-                    /* Unmonitor process */
-                    pusage.unmonitor(process.pid);
-
-                    callback(undefined,[
-                        cpuUsage,
-                        cpuFree
-                    ]);
+        if (os.platform() === 'win32') {
+            windowsCpu.nodeLoad(function(error, results) {
+                if(error) {
+                    callback(error);
                 }
-            }
-        });
+                    if (results === undefined) {
+                        callback('CPU status not found!');
+                    } else {
+                        let cpuUsage = parseInt(results.load);
+                        let cpuFree = 100 - cpuUsage;
+
+                        if (cpuUsage > 100) {
+                            cpuUsage = 100;
+                            cpuFree = 0;
+                        }
+
+                        callback(undefined,[
+                            cpuUsage,
+                            cpuFree
+                        ]);
+                    }
+            });
+        } else {
+            pusage.stat(process.pid, function(err, stat) {
+                if (err) {
+                    callback(err);
+                } else {
+                    if (stat.cpu === undefined) {
+                        callback('CPU status not found!');
+                    } else {
+                        let cpuUsage = parseInt(stat.cpu)
+                        let cpuFree = 100 - cpuUsage;
+
+                        if (cpuUsage > 100) {
+                            cpuUsage = 100;
+                            cpuFree = 0;
+                        }
+
+                        /* Unmonitor process */
+                        pusage.unmonitor(process.pid);
+
+                        callback(undefined,[
+                            cpuUsage,
+                            cpuFree
+                        ]);
+                    }
+                }
+            });
+        }
     };
 };
 
